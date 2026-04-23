c28e5f5901006fe174638908791d84d7201623dc
Меруерт Кожахмет <meruertkozahmet@MacBook-Air-Meruert.local>
2026-04-23 11:15:31 +0500

    add inner my_app folder

---
+++ my_app/lib/app_home work.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Text(
+        'Hello, Flutter!',
+        style: TextStyle(fontSize: 24),
+      ),
+      SizedBox(height: 20), // отступ
+      Image.network('https://flern.website/flutter-hello.png'),
+    ],
+  ),
+),
+      ),
+    );
+  }
+}
